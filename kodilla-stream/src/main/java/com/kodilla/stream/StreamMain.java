53e244b5a3f4e83517372668f0f0482db29109a1
Krzysztof.Kazula <kkazula92@gmail.com>
2022-01-23 15:22:27 +0100

    7.1 Beautifier of texts

--- kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -1,8 +1,33 @@
 package com.kodilla.stream;
 
+import com.kodilla.stream.beautifier.PoemBeautifier;
+import com.kodilla.stream.lambda.ExpressionExecutor;
+import com.kodilla.stream.reference.FunctionalCalculator;
+
 public class StreamMain {
 
     public static void main(String[] args) {
-        System.out.println("Welcome to module 7 - Stream");
+        ExpressionExecutor expressionExecutor = new ExpressionExecutor();
+
+        System.out.println("Calculating expressions with lambdas");
+        expressionExecutor.executeExpression(10, 5, (a, b) -> a + b);
+        expressionExecutor.executeExpression(10, 5, (a, b) -> a - b);
+        expressionExecutor.executeExpression(10, 5, (a, b) -> a * b);
+        expressionExecutor.executeExpression(10, 5, (a, b) -> a / b);
+
+        System.out.println("Calculating expressions with method references");
+        expressionExecutor.executeExpression(3, 4, FunctionalCalculator::addAToB);
+        expressionExecutor.executeExpression(3, 4, FunctionalCalculator::subBFromA);
+        expressionExecutor.executeExpression(3, 4, FunctionalCalculator::multiplyAByB);
+        expressionExecutor.executeExpression(3, 4, FunctionalCalculator::divideAByB);
+
+        //7.1Beautifier of texts
+        PoemBeautifier poemBeautifier = new PoemBeautifier();
+
+        poemBeautifier.beautify("First text to beautify", text -> "ABC" + text + "ABC");
+        poemBeautifier.beautify("Second text to beautify", text -> text.toUpperCase());
+        poemBeautifier.beautify("Third text to beautify", text -> text + "(length of the text:" + text.length() + ")");
+        poemBeautifier.beautify("Fourth text to beautify", text -> text.toLowerCase());
+
     }
 }
