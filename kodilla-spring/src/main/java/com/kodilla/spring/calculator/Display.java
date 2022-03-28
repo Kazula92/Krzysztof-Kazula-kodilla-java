b89b7b1e3c64dc383a25ef8ef34153f7bd8b3135
Krzysztof.Kazula <kkazula92@gmail.com>
2022-03-28 22:12:45 +0200

    14.2 Calculator with display

---
+++ kodilla-spring/src/main/java/com/kodilla/spring/calculator/Display.java
@@ -0,0 +1,11 @@
+package com.kodilla.spring.calculator;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public final class Display {
+
+    public double displayValue(double val){
+        return val;
+    }
+}
---
+++ kodilla-spring/src/test/java/com/kodilla/spring/calculator/CalculatorTestSuite.java
@@ -0,0 +1,24 @@
+package com.kodilla.spring.calculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalculatorTestSuite {
+
+    @Autowired
+    private Calculator calculator;
+
+    @Test
+    void testCalculations() {
+        //Given
+        //When
+        calculator.add(2.3,4.2);
+        calculator.sub(5.1, 2.1);
+        calculator.mul(3.0,1.5);
+        calculator.div(3.5,0.5);
+        //Then
+    }
+
+}
